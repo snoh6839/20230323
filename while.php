24a954373400e2b824e64d7d7634c23fd4b59d14
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-23 17:28:48 +0900

    별찍기

--- while.php
+++ while.php
@@ -10,11 +10,94 @@
     //     $i++;
     // };
 
-    for ($i=2; $i <10 ; $i++) { 
-        echo $i."단\n";
-        for ($j=1; $j <10 ; $j++) { 
-            echo $i."x".$j."=".($i*$j)."\n"; 
+    // for ($i=2; $i <10 ; $i++) { 
+    //     echo $i."단\n";
+    //     for ($j=1; $j <10 ; $j++) { 
+    //         echo $i."x".$j."=".($i*$j)."\n"; 
+    //     }
+    // }
+    
+    // $count = 0;
+    // do {
+    //     echo str_repeat("*", $count) . str_repeat(" ", $count) . "\n";
+    //     $count++;
+    // } while ($count < 6);
+
+    $rows = 5; 
+
+    for ($i = 1; $i <= $rows; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "*";
         }
+    echo "\n";
     }
+
+    for ($i = $rows; $i > 0; $i--) {
+        for ($j=0; $j < $i; $j++) { 
+            echo "*";
+        }
+    echo "\n";
+    }
+
+    for ($i = $rows; $i > 0; $i--) {
+        for ($j=1; $j < $i; $j++) { 
+            echo " ";
+        }
+        for ($j= $rows+1; $j > $i; $j--) { 
+            echo "*";
+        }
+    echo "\n";
+    }
+
+    for ($i = 1; $i <= $rows-1; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo " ";
+        }
+        for ($j= $rows; $j > $i; $j--) { 
+            echo "*";
+        }
+    echo "\n";
+    }
+
+    $rows2 = 5*2;
+
+    for ($i=1; $i < $rows2; $i+=2) { 
+        for ($j=$rows2-1; $j > $i; $j-=2) { 
+            echo " ";
+        }
+        for ($j=0; $j < $i; $j++) { 
+            echo "*";
+        }
+    echo "\n";
+    }
+
+    for ($i=1; $i < $rows2; $i+=2) { 
+        for ($j=1; $j < $i; $j+=2) { 
+            echo " ";
+        }
+        for ($j=$rows2; $j > $i; $j--) { 
+            echo "*";
+        }
+    echo "\n";
+    }
+
+
+
+        $left_eye = rand(0, 1) ? 'o' : '-';
+        $right_eye = rand(0, 1) ? 'o' : '-';
+        $tail = rand(0, 1) ? '>>' : '//';
+
+        echo "   /^\ ____/^\     \n";
+        echo " /            \\  \n";
+        echo "/// | | | | | \\\\ \n";
+        echo "|   {$left_eye}     {$right_eye}     |\n";
+        echo "| ===  ^   ===  |\n";
+        echo " \_____________/\n";
+        echo " |___/\-v-/\___|        \n";
+        echo "  \\ v  \ /  v /====={$tail}  \n";
+        echo "   \\_________/   \n";
+
+    
+
 
 ?>
